9e46bd049659836d0f84944ec7852f1b661979e1
wasan <twasanj@gmail.com>
2025-11-30 11:30:26 +0300

    g

--- hh/ContentView.swift
+++ hh/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
+//        .padding()
+        //hhhhhhhhhh
         .padding()
     }
 }
