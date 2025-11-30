5d2ccb0bae12744de8678f52d2aa5a94768d0f84
wasan <twasanj@gmail.com>
2025-11-30 11:25:35 +0300

    Initial Commit

---
+++ hh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hh
+//
+//  Created by wasan jayid althagafi on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hh/hhApp.swift
@@ -0,0 +1,17 @@
+//
+//  hhApp.swift
+//  hh
+//
+//  Created by wasan jayid althagafi on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct hhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
